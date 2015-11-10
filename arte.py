531333dab4507667276696877d43eb06773cf379
Matthieu Boileau <matthieu.boileau@math.unistra.fr>
2015-11-10 11:38:50 +0100

    Fix bugs on version type and file name (show all versions)

--- arte.py
+++ arte.py
@@ -13,13 +13,14 @@ import sys
 import urllib2
 import wget
 from os import environ
-      
+
+debug = False
 
 if __name__ == '__main__':
 
     Arte_url = easygui.enterbox('Enter an Arte+7 URL: ')
     if not Arte_url:
-        sys.exit(0)         
+        sys.exit(0)
     page = urllib2.urlopen(Arte_url)
     soup = BeautifulSoup(page.read(), "html.parser")
     
@@ -30,42 +31,53 @@ if __name__ == '__main__':
     urls = {}  # Dict to store available URLs
     f = urllib2.urlopen(jsonfile_url)
     data = json.load(f)  # Read JSON file
-
-    broadcast = data['videoJsonPlayer']['VTI'].encode('utf-8')  # Get broadcast name
+    # Get broadcast name
+    broadcast = data['videoJsonPlayer']['VTI'].encode('utf-8').rstrip()
     try:
-        title = data['videoJsonPlayer']['VSU'].encode('utf-8')  # Get episode name
+        # Get episode name
+        episode = data['videoJsonPlayer']['VSU'].encode('utf-8').rstrip()
+        title = "{} - {}".format(broadcast, episode)
     except:
-        title = ""    
+        title = broadcast
 
     iversion = 1
     version_choices = []
     
+    avail_versions = {}
     for key, val in data['videoJsonPlayer']['VSR'].items():
         if key[:5] == u'HTTP_':  # Filter HTTP URLs
-            if val['quality'][0:2] == u'HD':  # HD format only
-                version_string = "{} ({})".format(val['versionLibelle'],
-                                                val['quality'].replace(" ",""))
-                urls[version_string] = val['url']  # store URL
-                version_choices.append(version_string)  # store version names
+            avail_versions[val['quality']] = val['versionLibelle']
+            
+    for key, val in data['videoJsonPlayer']['VSR'].items():
+        if key[:5] == u'HTTP_':  # Filter HTTP URLs
+            version_string = "{} ({})".format(val['versionLibelle'],
+                                            val['quality'].replace(" ",""))
+            urls[version_string] = val['url']  # store URL
+            version_choices.append(version_string)  # store version names
     
-    msg = "Available versions of \"{} - {}\" for download" \
-            .format(broadcast, title)
+    msg = "Available versions of \"{}\" for download" \
+            .format(title)
+    if debug:
+        print msg
+        print version_choices
     version_choice = easygui.choicebox(msg=msg,
                                        title="Available versions",
                                        choices=version_choices)
-    
+    if not version_choice:
+        sys.exit(0)
+        
     url = urls[version_choice]
 
     file_extension = url.split('.')[-1]
-    file_name = "Arte-{}-{}-{}.{}".format(broadcast, title,
-                                    version_string.replace(" ","-"),
+    tmp_name = "Arte-{}-{}.{}".format(title,
+                                    version_choice.replace(" ","-"),
                                     file_extension)
     # Remove ":" and replace spaces (and duplicate spaces) by underscores
-    file_name_string = re.sub(' +','_',file_name.replace(":",""))
+    file_name = re.sub(' +','_',tmp_name.replace(":",""))
 
     # Destination directory is default user download directory
     directory = environ['HOME']+"/Downloads"
-    download_path = directory + "/" + file_name_string
+    download_path = directory + "/" + file_name
     
     msg = 'Download {} to {}?'.format(url, download_path)
     title = "Please Confirm"
